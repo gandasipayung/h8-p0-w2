0b6e711707b826b8938928931c95f13b5b76e17e
gandasipayung <gandasipayung14@gmail.com>
2019-10-23 15:19:33 +0700

    Upload exercise-8 dan exercise-9 dan modif exercise-1

---
+++ exercise-9.js
@@ -0,0 +1,33 @@
+console.log('Tugas 1');
+
+function shoutOut(halo = '"Halo Function!"'){
+  return halo;
+}
+
+console.log(shoutOut());
+//
+console.log('Tugas 2');
+function calculateMultiply(num1, num2){
+  var hasilPerkalian = num1 * num2
+  return hasilPerkalian;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+console.log('Tugas 3');
+function processSentence(){
+  var fullSentence ='"Nama saya ' + name + ', '+ 'umur saya '+ age +' tahun, '+ 'alamat saya di ' + address +', '+'dan saya punya hobby yaitu '+ hobby + '!"';
+  return fullSentence;
+}
+
+var name = 'as';
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence();
+console.log(fullSentence);
